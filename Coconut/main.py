0b461c3b04e96df167cdcb6786ca9a1174b3e606
Eunho Lee <hiveruby@gmail.com>
2023-12-27 19:54:04 +0900

    feat: Add many event functions
    - Coconut/main.py: create `on_ready`, `on_connect`, `on_disconnect`, `on_resumed` event methods; set up logger

--- Coconut/main.py
+++ Coconut/main.py
@@ -1,7 +1,31 @@
 import discord
+from . import log_manager
 
 client = discord.Client(intents=discord.Intents.all())
 
+logger = log_manager.get_logger()
+log_manager.set_logger(logger)
+
+
+@client.event
+async def on_ready():
+    logger.info("Ready")
+
+
+@client.event
+async def on_connect():
+    logger.info("Connected")
+
+
+@client.event
+async def on_disconnect():
+    logger.warning("Disconnect")
+
+
+@client.event
+async def on_resumed():
+    logger.info("Resumed")
+
 
 def run(token: str):
-    client.run(token)
+    client.run(token, log_handler=None)
